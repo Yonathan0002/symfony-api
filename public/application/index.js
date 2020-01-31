42c39154ec416d6794d2392723cc82eac585347d
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-01-31 12:13:04 +0100

    fetch terminer index.js bug a corriger

--- public/application/index.js
+++ public/application/index.js
@@ -1,1 +1,20 @@
-fetch()
+console.log("test");
+function updateList(url){
+    fetch(url)
+    .then((response)=>{
+        return response.json()
+    })
+    .then(function(data){
+        var liste = document.createElement("ul");
+        //console.log(data["hydra:member"]);
+        var element = document.createElement("li");
+        data["hydra:member"].forEach(race => {
+            element.innerHTML = race.name;
+            liste.appendChild(element);
+        })
+        document.body.appendChild(liste)
+    });
+}
+
+
+updateList("http://localhost:8000/api/races?page=1")
